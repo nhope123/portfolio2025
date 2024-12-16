a375b807227ed2b4b2dfefef1d035421b2d28bab
nhope <16958187+nhope123@users.noreply.github.com>
2024-12-16 10:11:51 -0500

    refactor: Add MUI theme and global styles to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
